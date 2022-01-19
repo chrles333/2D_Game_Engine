b4ef7de146e0d0bc82fe060cb302177a0c6e17b8
chrles333 <97774550+chrles333@users.noreply.github.com>
2022-01-19 17:53:41 +1100

    Delete Game_Engine/Src directory
    Rearranged Src folder

--- Game_Engine/Src/ECS/Components.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ECS.h"
-#include "TransformComponent.h"
-#include "SpriteComponent.h"
-#include "KeyboardController.h"
-#include "ColliderComponent.h"
-#include "TileComponent.h"
-
-// Will eventually create separate class for each component
